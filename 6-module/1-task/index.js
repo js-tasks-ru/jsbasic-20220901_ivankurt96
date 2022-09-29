0388e68fbf2bca669de7abea1be26545a7c6fa7b
Иван Куртц <ivankurt96@gmail.com>
2022-09-29 18:56:29 +0300

    6-module done

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,42 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+  this.elem = deleteRows(rows)
   }
+}
+function createTable(obj) {
+  return `
+  <tr>
+  <td>${obj.name}</td>
+  <td>${obj.age}</td>
+  <td>${obj.salary}</td>
+  <td>${obj.city}</td>
+  <td><button>X</button></td>
+</tr>
+  `
+}
+function makeHTML(array){
+  return `
+  <table>
+  <thead>
+        <tr>
+            <td>Имя</td>
+            <td>Возраст</td>
+            <td>Зарплата</td>
+            <td>Город</td>
+        </tr>
+    </thead>
+    <tbody>
+        ${array.map(createTable).join('')}
+    </tbody>
+    </table>`
+}
+function deleteRows(array){
+  const table = document.createElement("table");
+  table.innerHTML = makeHTML(array);
+  const buttons = table.querySelectorAll("button")
+  for (const button of buttons){
+    button.addEventListener('click', (event) =>
+    event.target.closest("tr").remove())
+  }
+  return table;
 }
